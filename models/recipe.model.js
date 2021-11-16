4d848d674305e73f23c4bbf7078c91976da8abd6
reyhanemyr <13519167@std.stei.itb.ac.id>
2021-11-17 01:12:39 +0700

    update api for adding recipes

--- models/recipe.model.js
+++ models/recipe.model.js
@@ -12,6 +12,7 @@ module.exports = (sequelize, DataTypes) => {
       name: {
         type: DataTypes.STRING,
         allowNull: false,
+        unique: true,
         validate: {
           notNull: { msg: 'Recipe must have a name' },
           notEmpty: { msg: 'Name must not be empty' },
--- routes/recipe.js
+++ routes/recipe.js
@@ -21,13 +21,34 @@ router.route('/names').get((req, res) => {
 });
 
 // POST
-// Create new recipe without ingredient.
+// Create new recipe.
 router.route('/').post((req, res) => {
   const name = req.body.name;
 
-  db.recipes.create({name: name})
-    .then((recipe) => {res.status(200).json({id: recipe.id})})
-    .catch(err => res.status(400).json({error: err}));
+  db.ingredients.findAll({attributes: ['id', 'name']})
+    .then((ingredients) => {
+      ingMap = Object.fromEntries(ingredients.map(e => [e.name, e.id]));
+      db.recipes.create({name: name})
+        .then((recipe) => {
+          const recipeIng = req.body.ingredients.map((ing) => {
+            return {id: recipe.id, ingredient: ingMap[ing.ingredient], count: ing.count}
+          });
+ 
+          db.recipeIngredients.bulkCreate(recipeIng)
+            .then(() => res.status(200).json({id: recipe.id}))
+            .catch(err => {
+              db.recipes.destroy({
+                where: {
+                  id: recipe.id
+                }
+              })
+                .then(() => res.status(400).json({error: err}));
+            })
+        })
+        .catch(err => res.status(400).json({error: err}));
+    })
+    .catch(err => res.status(400).json({error: err}))
+    
 });
 
 module.exports = router;
